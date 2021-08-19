b6070fa25709dd9ed4068eecdae219e95944e268
Charlotte Gayton <charlotte.gayton@gmail.com>
2021-08-19 16:57:31 +0100

    Fourth Submission

--- src/File/Program.cs
+++ src/File/Program.cs
@@ -9,7 +9,63 @@ namespace program{
     class Program
     {
         
+        public void TaskOnePartOne()
+        {
+            string folder = @"/Users/charlottegayton/temp/";
+            StreamReader read = new StreamReader(folder + "file.txt");
 
+            string linevalue = string.Empty;
+            List<int> values = new List<int>();
+
+            while ((linevalue = read.ReadLine()) != null)
+            {
+                int numline = Int32.Parse(linevalue);
+                values.Add(numline);
+            }
+
+            values.ForEach(Console.WriteLine);
+
+            foreach(int i in values){
+                int tolookfor = 2020 - i;
+                bool contains = values.Contains(tolookfor);
+                if(contains == true){
+                    Console.WriteLine("The two values that make 2020 are: {0} and: {1}", i, tolookfor);
+                    break;
+                }
+            }
+
+        }
+
+        public void TaskTwoPartOne()
+        {
+            string folder = @"/Users/charlottegayton/temp/";
+            StreamReader read = new StreamReader(folder + "filetwo.txt");
+
+            string linevalue = string.Empty;
+            List<String> timesAllowed = new List<String>();
+            List<String> letter = new List<String>();
+            List<String> password = new List<String>();
+            int counter = 0;
+
+            while ((linevalue = read.ReadLine()) != null)
+            {
+
+                string[] words = linevalue.Split(' ');
+                string[] numbers = words[0].Split('-');
+                int[] intnum = Array.ConvertAll(numbers, s => int.Parse(s));
+                words[1] = words[1].Remove(1);
+                string expression = words[1];
+                int numOfOccurances = Regex.Matches(words[2], expression).Count;
+
+                if( numOfOccurances <= intnum[1] && numOfOccurances >= intnum[0]){
+                    counter += 1;
+                }
+                
+            }
+
+            Console.WriteLine(counter); 
+
+        }
 
         public void TaskTwoPartTwo(){
 
@@ -50,63 +106,11 @@ namespace program{
 
         }
 
-        public void TaskOnePartOne()
-        {
-            string folder = @"/Users/charlottegayton/temp/";
-            StreamReader read = new StreamReader(folder + "file.txt");
-
-            string linevalue = string.Empty;
-            List<int> values = new List<int>();
-
-            while ((linevalue = read.ReadLine()) != null)
-            {
-                int numline = Int32.Parse(linevalue);
-                values.Add(numline);
-            }
-
-            values.ForEach(Console.WriteLine);
-
-            foreach(int i in values){
-                int tolookfor = 2020 - i;
-                bool contains = values.Contains(tolookfor);
-                if(contains == true){
-                    Console.WriteLine("The two values that make 2020 are: {0} and: {1}", i, tolookfor);
-                    break;
-                }
-            }
-
+        public void TaskThreePartThree(){
+            
         }
     
-        public void TaskTwoPartOne()
-        {
-            string folder = @"/Users/charlottegayton/temp/";
-            StreamReader read = new StreamReader(folder + "filetwo.txt");
-
-            string linevalue = string.Empty;
-            List<String> timesAllowed = new List<String>();
-            List<String> letter = new List<String>();
-            List<String> password = new List<String>();
-            int counter = 0;
-
-            while ((linevalue = read.ReadLine()) != null)
-            {
-
-                string[] words = linevalue.Split(' ');
-                string[] numbers = words[0].Split('-');
-                int[] intnum = Array.ConvertAll(numbers, s => int.Parse(s));
-                words[1] = words[1].Remove(1);
-                string expression = words[1];
-                int numOfOccurances = Regex.Matches(words[2], expression).Count;
-
-                if( numOfOccurances <= intnum[1] && numOfOccurances >= intnum[0]){
-                    counter += 1;
-                }
-                
-            }
-
-            Console.WriteLine(counter); 
-
-        }
+       
 
         static void Main(string[] args){
             Program p = new Program();
